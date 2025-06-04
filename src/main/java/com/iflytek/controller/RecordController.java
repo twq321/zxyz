2718d692ba3928dfbb31de0aecab9b5672c11f58
123456 <2371037642@qq.com>
2025-06-04 20:33:03 +0800

    新增了recordController

---
+++ src/main/java/com/iflytek/controller/RecordController.java
@@ -0,0 +1,19 @@
+package com.iflytek.controller;
+
+import com.iflytek.service.RecordService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/record")
+@Api(tags = "记录管理")
+public class RecordController {
+
+    @Autowired
+    private RecordService recordService;
+
+
+
+}
