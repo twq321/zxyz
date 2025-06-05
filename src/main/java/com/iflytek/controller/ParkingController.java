8574116a12701bd06681d98f80f25701043aded0
Jarvis Sun <13606879+jarvis-sun@user.noreply.gitee.com>
2025-06-05 09:46:07 +0800

    add parkingCURD

--- src/main/java/com/iflytek/controller/ParkingController.java
+++ src/main/java/com/iflytek/controller/ParkingController.java
@@ -34,23 +34,30 @@ public class ParkingController {
     public ResponseEntity<?> park(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestParam int parkingId, @RequestParam(required = false) int bikeId) {
         User user = userDetails.getUser();
         Parking parking = parkingService.selectById(parkingId);
+        if (parking.getStatus() == 1) {
+            return ResponseEntity.badRequest().body("操作失败, 车位已满.");
+        }
         int res = 0;
         if (user.getNowborrow() == 0) {
             // 存车
             res = parkingService.stParking(user, bikeId, parking);
         } else {
+            // 还车
             res = parkingService.deBorrow(user, parking);
         }
         if (res == 0) {
             return ResponseEntity.badRequest().body("操作失败.");
         } else {
-            return ResponseEntity.ok(res);
+            return ResponseEntity.ok("操作成功.");
         }
     }
     @ApiOperation("取车/借车")
     @PostMapping("/borrow")
     public ResponseEntity<?> borrow(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestParam int parkingId) {
         User user = userDetails.getUser();
+        if (user.getNowborrow() != 0) {
+            return ResponseEntity.badRequest().body("操作失败, 还有自行车未还.");
+        }
         Parking parking = parkingService.selectById(parkingId);
         if (parking.getBikeid() == 0) {
             return ResponseEntity.badRequest().body("操作失败, 车位为空.");
@@ -64,9 +71,6 @@ public class ParkingController {
             }
         }
         int res = 0;
-        if (user.getNowborrow() != 0) {
-            return ResponseEntity.badRequest().body("操作失败, 还有自行车未还.");
-        }
         if (isOwn != 0) {
             // 不是自己的车
             if (parking.getRecordid() != 0) {
