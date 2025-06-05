bdee7b16751275e388bb288cc820ce6e744abe7f
Jarvis Sun <13606879+jarvis-sun@user.noreply.gitee.com>
2025-06-05 13:12:27 +0800

    add RecordController

--- src/main/java/com/iflytek/controller/RecordController.java
+++ src/main/java/com/iflytek/controller/RecordController.java
@@ -1,8 +1,13 @@
 package com.iflytek.controller;
 
+import com.iflytek.dto.CustomUserDetails;
 import com.iflytek.service.RecordService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +19,9 @@ public class RecordController {
     @Autowired
     private RecordService recordService;
 
-
-
+    @ApiOperation("查询订单")
+    @GetMapping
+    public ResponseEntity<?> selectRecord(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(recordService.selectRecordsById(userDetails.getUser().getUserid()));
+    }
 }
--- src/main/java/com/iflytek/service/RecordService.java
+++ src/main/java/com/iflytek/service/RecordService.java
@@ -1,4 +1,9 @@
 package com.iflytek.service;
 
+import com.iflytek.entity.Record;
+
+import java.util.List;
+
 public interface RecordService {
+    List<Record> selectRecordsById(int userId);
 }
